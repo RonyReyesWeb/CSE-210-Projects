200082e20f2b2d34ad311bf4030d01fbae65561f
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 15:48:17 -0700

    creation of function 2

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -6,4 +6,18 @@ class Program
     {
 
     }
+
+    // Function 1: Display a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function 2: Ask for the user's name and return it
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
 }
