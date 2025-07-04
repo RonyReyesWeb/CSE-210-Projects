539d813f62cdae746ee297fb742936386e31f5cf
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:26:51 -0700

    implementing logic of determining letter on the numeric grade

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -16,9 +16,31 @@ class Program
         // Declare a variable to hold the letter grade
         string letter;
 
+        // Determine the letter grade based on the numeric grade
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
         //testing the results
         Console.WriteLine("You entered: " + grade);
-
+        Console.WriteLine("Your letter grade is: " + letter);
 
     }
 }
