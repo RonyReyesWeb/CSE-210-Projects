4e711806829a625780a3b19e2c78ad2bc0790084
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:15:44 -0700

    test week 1 exercise 1 pushing

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //test week 1 exercise 1 pushing
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
     }
 }
