a18675c2cd11a06244e239d1f01b68475435dd52
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:43:29 -0700

    adding the logic of it for user input

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,13 +5,19 @@ class Program
     static void Main()
     {
         //Step 1 no loop
-        
+
         // Ask user for the magic number
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
+        // Ask for the user's guess
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
         //testing the result
         Console.WriteLine($"Your letter grade is: {magicNumber}");
+        Console.WriteLine($"Your letter grade is: {guess}");
+
 
     }
 }
