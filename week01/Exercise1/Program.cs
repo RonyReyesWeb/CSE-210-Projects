ceb225229a4f6d90b13236e7110788431e1c292c
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:03:39 -0700

    adding prompting for last name and showing in additional line

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -8,8 +8,13 @@ class Program
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        // Output in specified format
-        Console.WriteLine($"Your name is {firstName}.");
 
+        // Prompt for first name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Output in specified format
+        Console.WriteLine($"Your first name  is {firstName}.");
+        Console.WriteLine($"Your last name  is {lastName}.");
     }
 }
