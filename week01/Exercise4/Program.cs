1adcf55cb8d636088a1b5716517eaadb704a95e6
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 15:01:27 -0700

    finishing Compute the sum, or total, of the numbers in the list

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -6,13 +6,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        // STEP 1: Get numbers from the user and store them in a list
-        
+        // STEP 1: Compute the sum, or total, of the numbers in the list.
+
         // Create a list to store the numbers
         List<int> numbers = new List<int>();
         int number;
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
+        // Loop to read numbers until user enters 0
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        // create the results
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        // Show the result from the results
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
