e90cdbe93c3ae3c58e678ec9ee5071b3100dfc91
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:14:21 -0700

    deleting editing wrong file (working on week 2 of the course when I am just in week 1)

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Testing exercise two pushing
         Console.WriteLine("Hello World! This is the Resumes Project.");
     }
 }
