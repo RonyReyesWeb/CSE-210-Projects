5cc1a49c7eabcc213f72ad5b4d5cef59d96adba2
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:00:58 -0700

    adding the first name and showing output to test the formula

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,9 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        //adding changes
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Prompt for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Output in specified format
+        Console.WriteLine($"Your name is {firstName}.");
+
     }
 }
