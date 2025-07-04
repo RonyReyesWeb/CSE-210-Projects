c3c56a018abaf107cac8f90d120ce2a10bfb8c6b
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:05:29 -0700

    finishing first exercise as requested

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -8,13 +8,11 @@ class Program
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-
         // Prompt for first name
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        // Output in specified format
-        Console.WriteLine($"Your first name  is {firstName}.");
-        Console.WriteLine($"Your last name  is {lastName}.");
+        // Output in specified format adding the comma and last name, then firs and last name.
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
