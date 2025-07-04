9b6d6df51515fed281dec696176130b8165331f4
Rony Reyes <rreyes@missionmcss.com>
2025-07-04 13:21:14 -0700

    testing results

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,9 +6,13 @@ class Program
     {
         // Prompt the user to enter their grade percentage
         Console.Write("Enter your grade percentage: ");
-        
+
         // Read the input from the user as a string
         string input = Console.ReadLine();
+
+        //testing the results
+        Console.WriteLine("You entered: " + input);
+
 
     }
 }
