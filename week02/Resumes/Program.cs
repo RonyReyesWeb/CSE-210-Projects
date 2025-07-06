1581ea3918d482b59637692f030b7b7e19958739
Rony Reyes <rony@caidci.com>
2025-07-06 14:06:42 -0700

    adding a line test

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program
 {
+    // Adding a test line to see if it will work doing it from here
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
