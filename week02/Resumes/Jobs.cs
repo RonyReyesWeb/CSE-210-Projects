068f484807f66bb9fd5c8f685dc490a0274d29ac
Rony Reyes <rreyes@missionmcss.com>
2025-07-07 10:27:40 -0700

    Create job class with variables

---
+++ week02/Resumes/Jobs.cs
@@ -0,0 +1,10 @@
+using System;
+
+// Step 1 Create Job class with member variables
+public class Job
+{
+    public string _jobTitle = "";
+    public string _company = "";
+    public int _startYear;
+    public int _endYear;
+}
