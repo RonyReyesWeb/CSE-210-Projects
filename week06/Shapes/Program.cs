7d9a7ade71dd5f38c4afb895f5c2e8cc431a957f
Rony Reyes <rony@caidci.com>
2025-08-06 17:21:54 -0700

    finalizing the program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create a list of Shape (polymorphic collection)
+        List<Shape> shapes = new List<Shape>();
+
+        // Add various shapes to the list
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        // Iterate through the shapes and display color and area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():0.00}");
+        }
     }
 }
